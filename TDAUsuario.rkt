5521ebe9650233754ad2138722925ef8f710a983
Matias Colil <matias.colil@usach.cl>
2021-11-20 17:28:32 -0300

    Cambios mayores en TDA Usuario
    se agregan funciones, se modifican funciones al TDA usuario

--- TDAUsuario.rkt
+++ TDAUsuario.rkt
@@ -1,12 +1,17 @@
 #lang racket
+(require  "TDAFecha.rkt")
+
 ;TDA usuario
+;TDA usuario que va a representar a un usuario de una plataforma de documentos colaborativos
+;Representacion: es una lista de 4 elementos que representan lo siguiente en este orden
+;[ nombre del usuario, contraseña del usuario, estado de actividad/inactividad, fecha de creacion del usuario]
 
 ;CONSTRUCTOR
 ;Funcion que me crea una lista de usuario
-;Dom: usuario x contraseña
+;Dom: fecha x usuario x contraseña
 ;Rec: una lista
-(define (crearLista-usuario usuario contraseña)
-  (list usuario contraseña)
+(define (crearLista-usuario usuario contraseña login fecha)
+  (list usuario contraseña login fecha);login es valor 0 si esta deslogeado o 1 si esta logeado
   )
 
 ;PERTENENCIA
@@ -14,9 +19,12 @@
 ;Dom: una lista
 ;Rec: valor booleano
 
-(define (esLista-usuarios? lista)
-  (if (and (string? (car lista))
-           (string? (car(cdr lista)))
+(define (esLista-usuarios? listaUsuario)
+  (if (and (string? (car listaUsuario))
+           (string? (car(cdr listaUsuario)))
+           (integer? (car(cdr(cdr listaUsuario))))
+           (esfecha? (car(cdr(cdr(cdr listaUsuario)))))
+           (=(length listaUsuario) 4)
            )
       #t
       #f
@@ -29,9 +37,9 @@
 ;Dom: lista
 ;Rec: string
 
-(define (getUsuario lista)
-  (if (list? lista)
-      (car lista)
+(define (getUsuario listaUsuario)
+  (if (esLista-usuarios? listaUsuario)
+      (car listaUsuario)
       #f
       )
   )
@@ -40,61 +48,196 @@
 ;Dom: lista
 ;Rec: string
 
-(define (getContraseña lista)
-  (if (list? lista)
-      (car (cdr lista))
+(define (getContraseña listaUsuario)
+  (if (esLista-usuarios? listaUsuario)
+      (car (cdr listaUsuario))
+      #f
+      )
+  )
+
+;Funcion que me retorna el valor de login del usuario
+;Dom: lista;Rec: string
+(define (getLogin listaUsuario)
+  (if (esLista-usuarios? listaUsuario)
+      (car (cdr (cdr listaUsuario)))
+      #f
+      )
+  )
+
+;Funcion que me retorna la fecha de creacion del usuarios
+;Dom: lista
+;Rec: fecha
+(define (getFecha-usuario listaUsuario)
+  (if (esLista-usuarios? listaUsuario)
+      (car (cdr (cdr (cdr listaUsuario))))
       #f
       )
   )
 
 ;MODIFICADORES
-;Funcion que me recibe un nuevo nombre y una lista de usuario y reemplaza el nombre de dicha lista
-;Dom: string x lista
-;Rec: una lista
 
-(define (setNombre lista nuevoNombre)
-  (if (list? lista)
+;Funcion que me recibe un nuevo nombre y una lista de usuario y reemplaza el nombre de dicha lista
+;Dom: list X string
+;Rec: list
+
+(define (setNombre listaUsuario nuevoNombre)
+  (if (list? listaUsuario)
       (if (string? nuevoNombre)
-          (list nuevoNombre (getContraseña lista))
-          #f
+          (list nuevoNombre (getContraseña listaUsuario) (getLogin listaUsuario) (getFecha-usuario listaUsuario))
+          listaUsuario
           )
-      #f
+      listaUsuario
       )
   )
 
-;Funcion que me recibe un nuevo nombre y una lista de usuario y reemplaza el nombre de dicha lista
-;Dom: string x lista
-;Rec: una lista
+;Funcion que me recibe una nueva contraseña y una lista de usuario y reemplaza la contraseña en dicha lista
+;Dom: list X string
+;Rec: list
 
-(define (setContraseña lista nuevaContraseña)
-  (if (list? lista)
+(define (setContraseña listaUsuario nuevaContraseña)
+  (if (list? listaUsuario)
       (if (string? nuevaContraseña)
-          (list (getUsuario lista) nuevaContraseña)
-          #f
+          (list (getUsuario listaUsuario) nuevaContraseña (getLogin listaUsuario) (getFecha-usuario listaUsuario))
+          listaUsuario
           )
-      #f
+      listaUsuario
       )
   )
+
+;Funcion que me recibe un nuevo estado de actividad/inactividad y una lista de usuario y lo reemplaza en dicha lista
+;Dom: list X string
+;Rec: list
+
+(define (setLogin listaUsuario nuevoLogin)
+  (if (list? listaUsuario)
+      (if (integer? nuevoLogin)
+          (list (getUsuario listaUsuario) (getContraseña listaUsuario) nuevoLogin (getFecha-usuario listaUsuario))
+          listaUsuario
+          )
+      listaUsuario
+      )
+  )
+
 
 ;OTRAS FUNCIONES
 
-;Funcion que me agrega una lista de usuario a una lista de usuarios ya existente
+;Funcion que me agrega una lista de usuario a una lista de listas usuarios ya existente
 ;Dom: list x list
 ;Rec: list
 
 (define (agregarUsuario lista nuevoUsuario-lista)
   (if (list? nuevoUsuario-lista)
       (if (esLista-usuarios? nuevoUsuario-lista)
-          (list lista nuevoUsuario-lista)
+          (append lista (list nuevoUsuario-lista))
           lista
           )
       lista
       )
   )
 
+;Funcion que me busca si ya existe este usuario
+;Dom: list x string
+;Rec: valor booleano
+;Se utiliza recursion natural
 
+(define (estaUsuario? lista-usuarios usuario)
+  (if (null? lista-usuarios)
+      #f
+      (if (string=? (getUsuario (car lista-usuarios)) usuario)
+          #t
+          (if (null? (cdr lista-usuarios))
+              #f
+              (estaUsuario? (cdr lista-usuarios) usuario)
+              )
+          )
+      )
+  )
 
+;Funcion que me verifica si un usuario en especifico esta logeado
+;Dom: list X string X string
+;Rec: valor booleano
+;Se utiliza recursion natural
 
+(define (estaLogeado? lista-usuarios nombre contraseña)
+  (if (null? lista-usuarios)
+      #f
+      (if (and (equal? (getUsuario (car lista-usuarios)) nombre)
+               (equal? (getContraseña (car lista-usuarios)) contraseña)
+               )
+          #t
+          (estaLogeado? (cdr lista-usuarios) nombre contraseña)
+          )
+      )
+  )
 
+;Funcion que me busca si existe algun usuario logeado
+;Dom: list
+;Rec: valor booleano
+(define (alguienLogeado? listaUsuarios)
+  (if (null? listaUsuarios)
+      #f
+      (if (eq? (getLogin (car listaUsuarios)) 1)
+          #t
+          (alguienLogeado? (cdr listaUsuarios))
+          )
+      )
+  )
+
+;Funcion que me retorna el nombre del usuario que esta logeado
+;Dom: list
+;Rec: string
+(define (usuarioLogeado lista-usuarios)
+  (if (eq? (getLogin (car lista-usuarios)) 1)
+      (getUsuario (car lista-usuarios))
+      (usuarioLogeado (cdr lista-usuarios))
+      )
+  )
+
+;Funcion que me retorna la lista de usuario de un usuario en especifico
+;Dom: list X string
+;Rec: list
+(define (listaUser-especifico listaUsuarios usuario)
+  (if (eq? (getUsuario (car listaUsuarios)) usuario)  
+      (car listaUsuarios)
+      (listaUser-especifico (cdr listaUsuarios) usuario)
+      )
+  )
+  
+
+;Funcion que retorna una lista modificada sin el usuario que se quiere logear
+;Dom: string X string X string
+;Rec: una lista
+;Se utiliza recursion de cola
+
+(define (eliminarUsuario listaUsuarios nombre acc)
+  (if (null? listaUsuarios)
+      acc
+      (if (equal? (getUsuario (car listaUsuarios)) nombre)
+          (eliminarUsuario (cdr listaUsuarios) nombre acc)
+          (eliminarUsuario (cdr listaUsuarios) nombre (append acc (list(car listaUsuarios))))
+          )
+      )
+  )
+  
+;Funcion que me crea una nueva lista de listas de usuario
+;Dom: list X string X string X integer
+;Rec: list
+
+(define (nuevaLista-usuarios listaUsuarios nombre estadoLog)
+   (append (eliminarUsuario listaUsuarios nombre (list)) (list (setLogin (listaUser-especifico listaUsuarios nombre) estadoLog)))
+  )
+
+;Funcion que me retorna un string con los datos del usuario
+;Dom: list X string
+;Rec: string
+(define (Usuario->String listaUsuarios usuario)
+  (cond
+    [(eq? (getUsuario (car listaUsuarios)) usuario) (string-append "Nombre de usuario: " (getUsuario (car listaUsuarios))"\n"
+                                                                   "Fecha de creacion usuario: " (string-join (map number->string (getFecha-usuario (car listaUsuarios)))) "\n"
+                                                                   )
+                                                    ]
+    [(Usuario->String (cdr listaUsuarios) usuario)]
+    )
+  )
 
 (provide (all-defined-out))
